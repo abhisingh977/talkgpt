c12072161861a8595d76b87b1a07ea52ae8cebce
Abhishek Singh <abhis1997singh@gmail.com>
2023-03-05 01:41:51 -0500

    chat workinggit status

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.8.0-slim
+
+# Copy local code to the container image
+COPY model /app/model
+COPY templates /app/templates
+COPY main.py /app/main.py
+COPY requirements.txt /app/requirements.txt
+# Sets the working directory
+WORKDIR /app
+
+# Upgrade PIP
+RUN pip install --upgrade pip
+RUN pip3 install torch==1.9.0+cpu torchvision==0.10.0+cpu torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+#Install python libraries from requirements.txt
+RUN pip install -r requirements.txt
+
+
+# Set $PORT environment variable
+ENV PORT 8080
+
+# Run the web service on container startup
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- main.py
+++ main.py
@@ -1,43 +1,47 @@
 from flask import Flask, jsonify, request, render_template
-# from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
+import os
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 app = Flask(__name__)
 
-# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-# model = GPT2LMHeadModel.from_pretrained("/home/abhishek/abhi/gita/iss/good_model/checkpoint-4000-1")
+tokenizer = GPT2Tokenizer.from_pretrained("/app/model/")
+model = GPT2LMHeadModel.from_pretrained("/app/model/")
 #     # "/home/abhishek/abhi/gita/iss/good_model/checkpoint-3000-2"
 
-max_length = 75
+max_length = 60
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-# @app.route("/process-message", methods=["POST"])
-# def process_text():
-#     text = request.json["message"]
-#     # response = {'message': 'I received your message: ' + text}
-#     print(text)
+@app.route("/process-message", methods=["POST"])
+def process_text():
+    chatbot_response = ""
+    if data['response'] != '':
+        chatbot_response = data['response']
+
+    elif data['response'] == '':
+        user_input = chatbot_response + data['message']
+        print(user_input)
+        inputs = tokenizer.encode(user_input, return_tensors="pt")
+        outputs = model.generate(
+            inputs,
+            do_sample=True,
+            max_new_tokens=max_length,
+            pad_token_id=model.config.eos_token_id,
+            top_k=30,
+            early_stopping=True,
+            top_p=0.95,
+            repetition_penalty=20.0,
+        )
+        generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 
-#     inputs = tokenizer.encode(text, return_tensors="pt")
-#     outputs = model.generate(
-#         inputs,
-#         do_sample=True,
-#         max_new_tokens=max_length,
-#         pad_token_id=model.config.eos_token_id,
-#         top_k=50,
-#         early_stopping=True,
-#         top_p=0.95,
-#         repetition_penalty=50.0,
-#     )
+        data = {"message": generated_text[len(text):]}
 
-#     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-#     data = {"message": generated_text[len(text):]}
-
-    # return jsonify(data)
+    return jsonify(data)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
