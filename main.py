f803f90411b8c0988d292546e17da3c46cc824c4
Abhishek Singh <abhis1997singh@gmail.com>
2023-03-05 17:56:03 -0500

    working

--- main.py
+++ main.py
@@ -3,43 +3,39 @@ import os
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 app = Flask(__name__)
 
-tokenizer = GPT2Tokenizer.from_pretrained("/app/model/")
-model = GPT2LMHeadModel.from_pretrained("/app/model/")
+tokenizer = GPT2Tokenizer.from_pretrained("model/")
+model = GPT2LMHeadModel.from_pretrained("model/")
 #     # "/home/abhishek/abhi/gita/iss/good_model/checkpoint-3000-2"
 
-max_length = 60
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/process-message", methods=["POST"])
-def process_text():
-    chatbot_response = ""
-    if data['response'] != '':
-        chatbot_response = data['response']
+@app.route('/chat', methods=['POST'])
+def chat():
+    max_length = 50
 
-    elif data['response'] == '':
-        user_input = chatbot_response + data['message']
-        print(user_input)
-        inputs = tokenizer.encode(user_input, return_tensors="pt")
-        outputs = model.generate(
-            inputs,
-            do_sample=True,
-            max_new_tokens=max_length,
-            pad_token_id=model.config.eos_token_id,
-            top_k=30,
-            early_stopping=True,
-            top_p=0.95,
-            repetition_penalty=20.0,
-        )
-        generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-
-        data = {"message": generated_text[len(text):]}
-
-    return jsonify(data)
+    data = request.json
+    text = data.get('text')
+    print(text)
+    inputs = tokenizer.encode(text, return_tensors="pt")
+    outputs = model.generate(
+    inputs,
+    do_sample=True,
+    max_new_tokens=max_length,
+    pad_token_id=model.config.eos_token_id,
+    top_k=20,
+    early_stopping=True,
+    top_p=0.95,
+    repetition_penalty=20.0,
+    )
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(generated_text[len(text):])
+    # Return the response as JSON
+    return jsonify({'response': generated_text[len(text):]})
 
 
 if __name__ == "__main__":
